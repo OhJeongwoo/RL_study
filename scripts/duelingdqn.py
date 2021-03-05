5895c62d8a2c6f8b7127c2d963b152cb12a8df79
Janice <janice.jeong9902@gmail.com>
2021-03-05 15:24:39 +0900

    network architecture changed: reduced layers

--- scripts/duelingdqn.py
+++ scripts/duelingdqn.py
@@ -16,10 +16,10 @@ class DuelingDQN(nn.Module):
         self.h_layer1 = nn.Linear(self.inputs, self.hidden1)
         self.h_layer2 = nn.Linear(self.hidden1,self.hidden2)
 
-        self.fc1 = nn.Linear(self.hidden2//2, 256)
-        self.fc2 = nn.Linear(256, 64)
-        self.valuelayer = nn.Linear(64,3)
-        self.actionlayer = nn.Linear(64,1)
+        self.fc1 = nn.Linear(self.hidden2//2, 8)
+        # self.fc2 = nn.Linear(, 64)
+        self.valuelayer = nn.Linear(8,3)
+        self.actionlayer = nn.Linear(8,1)
 
     def forward(self, state):
         x = F.relu(self.h_layer1(state))
@@ -27,8 +27,8 @@ class DuelingDQN(nn.Module):
         x1, x2 = torch.split(x, self.hidden2 // 2, dim = 1)
         # x1 = x1.unsqueeze(0)
         # x2 = x2.unsqueeze(0)
-        self.value = F.relu(self.valuelayer(F.relu(self.fc2(F.relu(self.fc1(x1))))))
-        self.action = F.relu(self.actionlayer(F.relu(self.fc2(F.relu(self.fc1(x1))))))
+        self.value = F.relu(self.valuelayer(F.relu((self.fc1(x1)))))
+        self.action = F.relu(self.actionlayer(F.relu(self.fc1(x1))))
         
         self.q_val = self.value + self.action - torch.mean(self.action)
 
